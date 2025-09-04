cd0b7e9b10ec37017ba5696dbafcc081d044ea98
Josewer <josewer>
2025-09-04 12:59:01 +0200

    Se crean los cluster

--- back/app.js
+++ back/app.js
@@ -4,20 +4,17 @@ import logger from "morgan";
 import cors from "cors";
 import config from "./config.js";
 
-const app = express(); 
-const port = config.app.port
-app.disable("x-powered-by")
+const app = express();
+const port = config.app.port;
+
+app.disable("x-powered-by");
 app.use(json());
 
-console.clear();
-app.use(logger('dev'))
+app.use(logger("dev"));
 
 app.use(cors({
     origin: function (origin, callback) {
-
-        // Allow requests without origin (postman)
         if (!origin) return callback(null, true);
-
         if (config.cors.validOrigins.includes(origin)) {
             callback(null, true);
         } else {
@@ -28,10 +25,16 @@ app.use(cors({
     allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
+app.use((req, res, next) => {
+    console.log(`Worker ${process.pid} atendiendo ${req.method} ${req.url}`);
+     res.setHeader("X-Worker-PID", process.pid);
+    next();
+});
+
 app.use("/exercises", exercisesRouter);
 
 app.use((req, res) => {
     return res.status(404).end("<h1>Page not found<h1>");
-})
+});
 
-app.listen(port, () => { console.log(`Application running at http://localhost:${port}`) })
+export { app, port };
---
+++ back/server.js
@@ -0,0 +1,31 @@
+import cluster from "cluster";
+import os from "os";
+import dotenv from "dotenv";
+import { app, port } from "./app.js"
+
+if (cluster.isPrimary) {
+    console.clear();
+    console.log(`Master ${process.pid} is running`);
+    dotenv.config();
+    const numCPUs = os.cpus().length;
+
+    // Forzar round-robin
+    cluster.schedulingPolicy = cluster.SCHED_RR;
+
+    // Crear un worker por cada núcleo
+    for (let i = 0; i < numCPUs; i++) {
+        cluster.fork()
+    }
+
+
+    // Reemplazar workers que mueran
+    cluster.on("exit", (worker, code, signal) => {
+        console.log(`Worker ${worker.process.pid} died`);
+        cluster.fork();
+    });
+
+} else {
+    app.listen(port, () => {
+        console.log(`Worker ${process.pid} listening on http://localhost:${port}`);
+    });
+}
